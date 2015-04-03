4cb08cf5c2255ae9266db49dd56c3ab7371ebe62
Sebastian Werner <Plastefuchs@gmail.com>
2015-04-03 15:43:27 +0200

    Add basic package example
    Remove minion keys

--- salt-master-bootstrap.sh
+++ salt-master-bootstrap.sh
@@ -1,4 +1,11 @@
+# bootstrap the master
 curl -L http://bootstrap.saltstack.org | sudo sh -s -- -M
+
+# generate minion key and copy it to the vagrant dir for them minion to reach
 sudo salt-key --gen-keys=minion
 sudo cp minion.pub /etc/salt/pki/master/minions/minion
 sudo cp minion.p* /vagrant/
+
+# create salt server dir
+
+sudo ln -s /vagrant/salt /srv/salt
